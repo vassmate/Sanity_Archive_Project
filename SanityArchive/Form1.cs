099a515a073212c80ced1d57c9b1e592bc54191d
Zsori Lukacs Peter <zsori.lukacs@gmail.com>
2016-04-20 13:40:45 +0200

    GUI is completed (excl. popup windows)

---
+++ SanityArchive/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SanityArchive
+{
+    public partial class SanityArchiveForm : Form
+    {
+        public SanityArchiveForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SanityArchiveForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
