73fe4b12ad0056f4048aeabd6fe7c923a1416558
szekelypeter <szekely.pter.29@gmail.com>
2016-04-21 12:08:54 +0200

    encrypter and compresser class is ready

--- SanityArchive/DeCompressor.cs
+++ SanityArchive/DeCompressor.cs
@@ -3,10 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
 
 namespace SanityArchive
 {
     class DeCompressor
     {
+        public static void Compress(string input)
+        {
+            FileInfo fileToCompress = new FileInfo(input);
+            FileStream originalFileStream = fileToCompress.OpenRead();
+            using (FileStream compressedFileStream = File.Create(fileToCompress.FullName + ".gz"))
+            {
+                using (GZipStream compressionStream = new GZipStream(compressedFileStream,
+                    CompressionMode.Compress))
+                {
+                    originalFileStream.CopyTo(compressionStream);
+
+                }
+            }
+            FileInfo info = new FileInfo(input + ".gz");
+            Console.WriteLine("Compressed {0} from {1} to {2} bytes.",
+            fileToCompress.Name, fileToCompress.Length.ToString(), info.Length.ToString());
+            
+            originalFileStream.Close();
+            fileToCompress.Delete();
+        }
+
+        public static void Decompress(string input)
+        {
+            FileInfo fileToDecompress = new FileInfo(input);
+            using (FileStream originalFileStream = fileToDecompress.OpenRead())
+            {
+                string currentFileName = fileToDecompress.FullName;
+                string newFileName = currentFileName.Remove(currentFileName.Length - fileToDecompress.Extension.Length);
+
+                using (FileStream decompressedFileStream = File.Create(newFileName))
+                {
+                    using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(decompressedFileStream);
+                    }
+                }
+            }
+            fileToDecompress.Delete();
+        }
     }
 }
--- SanityArchive/EnDecrypter.cs
+++ SanityArchive/EnDecrypter.cs
@@ -3,10 +3,99 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace SanityArchive
 {
     class EnDecrypter
     {
+        static string key = "ütvefúró";
+
+        public static void encrypt(string input)
+        {            
+                string strHash = key;
+                FileStream inStream, outStream;
+                CryptoStream CryStream;
+                TripleDESCryptoServiceProvider TDC = new TripleDESCryptoServiceProvider();
+                MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+
+                byte[] byteHash, byteTexto;
+                string output = input + ".cry";
+                inStream = new FileStream(input, FileMode.Open, FileAccess.Read);
+                outStream = new FileStream(output, FileMode.OpenOrCreate, FileAccess.Write);
+
+                byteHash = md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(strHash));
+                byteTexto = File.ReadAllBytes(input);
+
+                md5.Clear();
+
+                TDC.Key = byteHash;
+                TDC.Mode = CipherMode.ECB;
+
+                CryStream = new CryptoStream(outStream, TDC.CreateEncryptor(), CryptoStreamMode.Write);
+
+                int bytesRead;
+                long length, position = 0;
+                length = inStream.Length;
+
+                while (position < length)
+                {
+                    bytesRead = inStream.Read(byteTexto, 0, byteTexto.Length);
+                    position += bytesRead;
+
+                    CryStream.Write(byteTexto, 0, bytesRead);
+                }
+
+                inStream.Close();
+                outStream.Close();
+            FileInfo file = new FileInfo(input);
+            file.Delete();
+
+        }
+
+        public static void decrypt(string input)
+        {
+            string strHash = key;
+            FileStream inStream, outStream;
+            CryptoStream CryStream;
+            TripleDESCryptoServiceProvider TDS = new TripleDESCryptoServiceProvider();
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+
+            byte[] byteHash, byteTexto;
+            string output = input.Remove(input.Length - 3);
+            inStream = new FileStream(input, FileMode.Open, FileAccess.Read);
+            outStream = new FileStream(output, FileMode.OpenOrCreate, FileAccess.Write);
+
+            byteHash = md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(strHash));
+            byteTexto = File.ReadAllBytes(input);
+
+            md5.Clear();
+
+            TDS.Key = byteHash;
+            TDS.Mode = CipherMode.ECB;
+
+            CryStream = new CryptoStream(outStream, TDS.CreateDecryptor(), CryptoStreamMode.Write);
+
+            int bytesRead;
+            long length, position = 0;
+            length = inStream.Length;
+
+            while (position < length)
+            {
+                bytesRead = inStream.Read(byteTexto, 0, byteTexto.Length);
+                position += bytesRead;
+
+                CryStream.Write(byteTexto, 0, bytesRead);
+            }
+
+            inStream.Close();
+            outStream.Close();
+            FileInfo file = new FileInfo(input);
+            file.Delete();
+
+        }
     }
 }
